ea8c21cbe687f21745bd6d7929f2373d4795019e
Angellyn218 <angellyncervantes@yahoo.com>
2022-11-08 20:03:31 -0800

    Added dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,11 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        const postData = await Post.findAll();
+        const postData = await Post.findAll({
+            order: [
+                ['date_created', 'DESC']
+            ]
+        });
 
         const posts = postData.map((post) => post.get({ plain: true }));
 
@@ -14,6 +18,34 @@ router.get('/', async (req, res) => {
             page_name: "The Tech Blog",
             title: "The Tech Blog"
         })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ 
+                model: Post, 
+                order: [
+                    ['date_created', 'DESC']
+                ]
+            }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        const page_name = user.username + "'s Dashboard";
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true,
+            page_name: page_name,
+            title: "Dashboard"
+        });
+
     } catch (err) {
         res.status(500).json(err);
     }
