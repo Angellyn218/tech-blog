4ea43278733e5c1dbc78cdb80a342536d94fc58b
Angellyn218 <angellyncervantes@yahoo.com>
2022-11-08 19:45:17 -0800

    Added home route

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts, 
+            logged_in: req.session.logged_in,
+            page_name: "The Tech Blog",
+            title: "The Tech Blog"
+        })
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
