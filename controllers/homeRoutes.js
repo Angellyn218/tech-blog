845e0e02ec71415f165343a2936092573c215f8a
Angellyn218 <angellyncervantes@yahoo.com>
2022-11-08 20:22:06 -0800

    Added signup rendering route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,6 +62,19 @@ router.get('/login', (req, res) => {
         page_name: "Login",
         title: "The Tech Blog"
     });
-  });
+});
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('signup', {
+        page_name: "Sign Up",
+        title: "The Tech Blog"
+    });
+});
 
 module.exports = router;
